471b55fec4bde2cdfeac889b51abd57f1e7db428
ismailchaifi <ismailchaifi027@gmail.com>
2024-02-19 20:32:15 +0100

    addition spring framework

--- src/main/java/org/example/metier/MetierImpl.java
+++ src/main/java/org/example/metier/MetierImpl.java
@@ -1,14 +1,15 @@
 package org.example.metier;
 
 import org.example.dao.IDao;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service("metier")
 public class MetierImpl implements IMetier {
+
     private IDao dao;
 
-    public MetierImpl() {
-    }
-
-    public MetierImpl(IDao dao) {
+    public MetierImpl(@Qualifier("dao") IDao dao) {
         this.dao = dao;
     }
 
---
+++ src/main/java/org/example/pres/PresSpringAnnot.java
@@ -0,0 +1,14 @@
+package org.example.pres;
+
+import org.example.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PresSpringAnnot {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext("org.example.dao", "org.example.metier");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println(metier.calcul());
+
+    }
+}
