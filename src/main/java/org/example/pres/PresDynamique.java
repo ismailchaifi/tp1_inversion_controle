32888f4dce4e58c2d7f783767d85874ce20a6019
ismailchaifi <ismailchaifi027@gmail.com>
2024-02-19 11:36:10 +0100

    initial commit

---
+++ src/main/java/org/example/pres/PresDynamique.java
@@ -0,0 +1,27 @@
+package pres;
+
+import dao.IDao;
+import metier.IMetier;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+public class PresDynamique {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("config.txt"));
+
+        String daoClassName = sc.nextLine();
+        Class<?> cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.getConstructor().newInstance();
+
+        String metierClassName = sc.nextLine();
+        Class<?> cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+
+        Method method = cMetier.getMethod("setDao", IDao.class);
+        method.invoke(metier, dao);
+
+        System.out.println(metier.calcul());
+    }
+}
---
+++ src/main/java/org/example/pres/PresStatique.java
@@ -0,0 +1,13 @@
+package pres;
+
+import dao.DaoImpl;
+import metier.MetierImpl;
+
+public class PresStatique {
+    public static void main(String[] args) {
+        DaoImpl dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println(metier.calcul());
+    }
+}
