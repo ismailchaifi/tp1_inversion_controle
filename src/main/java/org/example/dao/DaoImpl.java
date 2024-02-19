584a0ee4b8da956591d5d88057b90596809dcc4b
ismailchaifi <ismailchaifi027@gmail.com>
2024-02-19 20:25:51 +0100

    addition spring framework

--- src/main/java/org/example/dao/DaoImpl.java
+++ src/main/java/org/example/dao/DaoImpl.java
@@ -1,5 +1,8 @@
 package org.example.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("dao")
 public class DaoImpl implements IDao {
 
     @Override
--- src/main/java/org/example/ext/DaoImpl2.java
+++ src/main/java/org/example/ext/DaoImpl2.java
@@ -1,7 +1,9 @@
 package org.example.ext;
 
 import org.example.dao.IDao;
+import org.springframework.stereotype.Repository;
 
+@Repository("dao2")
 public class DaoImpl2 implements IDao {
     @Override
     public double getData() {
